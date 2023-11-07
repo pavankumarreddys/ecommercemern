7a576fd4efdc60bf973a2b4f6d859777bcd36332
Pavan Kumar Reddy Sannala <pavanreddyr34@gmail.com>
2023-11-07 17:42:41 +0530

    ad

--- server.js
+++ server.js
@@ -5,11 +5,17 @@ import connectDb from './config/db.js';
 import authRoutes from './routes/authRoute.js'
 import cors from 'cors'
 import path from 'path'
+import {fileURLToPath} from 'url'
 //configure env
 dotenv.config();
 
 //database config
 connectDb();
+
+
+//esmodulefix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //rest object 
 const app = express();
